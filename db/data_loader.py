3b87cef1d512056140fce8539faa73f57e1055c2
Sofiia Yesakova <sonjaslonja@gmail.com>
2026-06-11 10:38:18 +0200

    Add data loader for database connection

---
+++ db/data_loader.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+# загрузка переменных окружения
+load_dotenv()
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db = os.getenv("DB_NAME")
+
+# подключение к PostgreSQL
+engine = create_engine(
+    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
+)
+
+def load_data():
+    df = pd.read_sql("SELECT * FROM sales", engine)
+    return df
