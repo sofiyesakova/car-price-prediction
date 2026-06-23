ed5027c78fd386e7b547d276d120c8bb0b9b4d1d
unknown <svitlanaqatest@gmail.com>
2026-06-23 12:15:59 +0200

    Fix pipeline + RF + DB fix

--- multi_pipeline/db.py
+++ multi_pipeline/db.py
@@ -7,38 +7,81 @@ engine = create_engine(
 )
 
 
-def save_to_db(data, price, satisfaction):
+def save_to_db(
+    data,
+    price,
+    satisfaction,
+    category_2,
+    category_3,
+    datum
+):
 
     query = text("""
         INSERT INTO email_predictions(
             datum,
+
             marke,
             modell,
             kraftstoff,
             getriebe,
-            hubraum_l,
+            bundesland,
+
             verkaufszahl,
-            kundenzufriedenheit,
+            hubraum_l,
+
+            jahr,
+            monat,
+            wochentag,
+
             predicted_price,
-            predicted_zufriedenheit
+            predicted_zufriedenheit,
+            predicted_category_2,
+            predicted_category_3
         )
         VALUES(
             :datum,
+
             :marke,
             :modell,
             :kraftstoff,
             :getriebe,
-            :hubraum_l,
+            :bundesland,
+
             :verkaufszahl,
-            :kundenzufriedenheit,
+            :hubraum_l,
+
+            :jahr,
+            :monat,
+            :wochentag,
+
             :price,
-            :satisfaction
+            :satisfaction,
+            :category_2,
+            :category_3
         )
     """)
 
+    payload = {
+        "datum": datum,
+
+        "marke": data.get("marke"),
+        "modell": data.get("modell"),
+        "kraftstoff": data.get("kraftstoff"),
+        "getriebe": data.get("getriebe"),
+        "bundesland": data.get("bundesland"),
+
+        "verkaufszahl": data.get("verkaufszahl"),
+        "hubraum_l": data.get("hubraum_l"),
+
+        "jahr": data.get("jahr"),
+        "monat": data.get("monat"),
+        "wochentag": data.get("wochentag"),
+
+        "price": price,
+        "satisfaction": satisfaction,
+        "category_2": category_2,
+        "category_3": category_3
+    }
+
     with engine.begin() as conn:
-        conn.execute(query, {
-            **data,
-            "price": price,
-            "satisfaction": satisfaction
-        })
+        conn.execute(query, payload)
